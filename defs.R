43c1636d2bea1845c28558528cd8a9312be9c33f
danielturek <dturek@berkeley.edu>
2015-07-07 13:38:06 -0700

    updates to everything

--- defs.R
+++ defs.R
@@ -130,59 +130,71 @@ resultsObjectClassDef <- R6Class(
             self$niter <- niter
         },
         run = function(name, mod, MCMCs, monitors=character(), MCMCnames) {
-            if(is.list(mod)) {
-                suiteOut <- MCMCsuite(
-                    mod$code, mod$constants, mod$data, mod$inits,
-                    MCMCs = MCMCs,
-                    niter = self$niter,
-                    monitors = monitors,
-                    summaryStats = self$statNames,
-                    makePlot = FALSE)
-                if(is.null(self$out[[name]])) self$out[[name]] <- self$newListEntry(dimnames(suiteOut$summary)[[3]])
-                MCMCnamesToUse <- if(!missing(MCMCnames)) MCMCnames else MCMCs
-                newTimings <- suiteOut$timing[MCMCs]
-                names(newTimings) <- MCMCnamesToUse
-                self$out[[name]]$timing <- c(self$out[[name]]$timing, newTimings)
-                newSummary <- suiteOut$summary[, self$statNames0, , drop=FALSE]
-                dimnames(newSummary)[[1]] <- MCMCnamesToUse
-                self$out[[name]]$summary <- abind(self$out[[name]]$summary, newSummary, along=1)
-                ar <- self$out[[name]]$ESS
-                arE <- self$out[[name]]$Efficiency
-                newAr <- ar
-                newArE <- arE
-                for(mcmc in MCMCs) {
-                    newAr <- rbind(newAr, suiteOut$summary[mcmc, 'effectiveSize',])
-                    newArE <- rbind(newArE, suiteOut$summary[mcmc, 'effectiveSize',]/suiteOut$timing[mcmc])
-                }
-                dimnames(newAr) <- list(c(dimnames(ar)[[1]],MCMCnamesToUse), dimnames(ar)[[2]])
-                dimnames(newArE) <- list(c(dimnames(arE)[[1]],MCMCnamesToUse), dimnames(arE)[[2]])
-                self$out[[name]]$ESS <- newAr
-                self$out[[name]]$Efficiency <- newArE
-                return(invisible(NULL))
-            } else if(is.function(mod)) {
-                ret <- mod(niter=self$niter)
-                ## fix jag's param names: x[1,2,3] -> x[1, 2, 3]
-                jagsNames <- dimnames(ret$summary)[[2]]
-                jagsNamesFixed <- gsub(',', ', ', jagsNames)
-                dimnames(ret$summary)[[2]] <- jagsNamesFixed
-                MCMCnamesToUse <- if(!missing(MCMCnames)) MCMCnames else stop('must provide MCMCnames')
-                self$out[[name]]$timing <- c(self$out[[name]]$timing, ret$theTime)
-                names(self$out[[name]]$timing)[length(self$out[[name]]$timing)] <- MCMCnamesToUse
-                alreadyParamNames <- dimnames(self$out[[name]]$summary)[[3]]
-                newSummary <- ret$summary[self$statNames0,alreadyParamNames]  ## reorder
-                newSummary3D <- array(as.numeric(newSummary), c(1,dim(newSummary)[1],dim(newSummary)[2]))
-                dimnames(newSummary3D) <- list(MCMCnamesToUse, self$statNames0, alreadyParamNames)
-                self$out[[name]]$summary <- abind(self$out[[name]]$summary, newSummary3D, along=1)
-                newESS <- ret$summary['effectiveSize',]
-                newESS <- newESS[alreadyParamNames]    ## reorder
-                oldMCMCnames <- dimnames(self$out[[name]]$ESS)[[1]]
-                self$out[[name]]$ESS <- rbind(self$out[[name]]$ESS, newESS)
-                dimnames(self$out[[name]]$ESS)[[1]] <- c(oldMCMCnames, MCMCnamesToUse)
-                newE <- newESS / ret$theTime   ## newESS already reordered
-                self$out[[name]]$Efficiency <- rbind(self$out[[name]]$Efficiency, newE)
-                dimnames(self$out[[name]]$Efficiency)[[1]] <- c(oldMCMCnames, MCMCnamesToUse)
-                return(invisible(NULL))
-            } else stop('bad first argument')
+            MCMCsToPrint <- paste0(if(!missing(MCMCnames)) MCMCnames else MCMCs, collapse=', ')
+            message('*******************************************************')
+            message('*******************************************************')
+            message('name:  ', name)
+            message('MCMCs: ', MCMCsToPrint)
+            t <- system.time({
+                if(is.list(mod)) {
+                    suiteOut <- MCMCsuite(
+                        mod$code, mod$constants, mod$data, mod$inits,
+                        MCMCs = MCMCs,
+                        niter = self$niter,
+                        monitors = monitors,
+                        summaryStats = self$statNames,
+                        makePlot = FALSE)
+                    if(is.null(self$out[[name]])) self$out[[name]] <- self$newListEntry(dimnames(suiteOut$summary)[[3]])
+                    MCMCnamesToUse <- if(!missing(MCMCnames)) MCMCnames else MCMCs
+                    newTimings <- suiteOut$timing[MCMCs]
+                    names(newTimings) <- MCMCnamesToUse
+                    self$out[[name]]$timing <- c(self$out[[name]]$timing, newTimings)
+                    newSummary <- suiteOut$summary[, self$statNames0, , drop=FALSE]
+                    dimnames(newSummary)[[1]] <- MCMCnamesToUse
+                    self$out[[name]]$summary <- abind(self$out[[name]]$summary, newSummary, along=1)
+                    ar <- self$out[[name]]$ESS
+                    arE <- self$out[[name]]$Efficiency
+                    newAr <- ar
+                    newArE <- arE
+                    for(mcmc in MCMCs) {
+                        newAr <- rbind(newAr, suiteOut$summary[mcmc, 'effectiveSize',])
+                        newArE <- rbind(newArE, suiteOut$summary[mcmc, 'effectiveSize',]/suiteOut$timing[mcmc])
+                    }
+                    dimnames(newAr) <- list(c(dimnames(ar)[[1]],MCMCnamesToUse), dimnames(ar)[[2]])
+                    dimnames(newArE) <- list(c(dimnames(arE)[[1]],MCMCnamesToUse), dimnames(arE)[[2]])
+                    self$out[[name]]$ESS <- newAr
+                    self$out[[name]]$Efficiency <- newArE
+                } else if(is.function(mod)) {
+                    ret <- mod(niter=self$niter)
+                    ## fix jag's param names: x[1,2,3] -> x[1, 2, 3]
+                    jagsNames <- dimnames(ret$summary)[[2]]
+                    jagsNamesFixed <- gsub(',', ', ', jagsNames)
+                    dimnames(ret$summary)[[2]] <- jagsNamesFixed
+                    MCMCnamesToUse <- if(!missing(MCMCnames)) MCMCnames else stop('must provide MCMCnames')
+                    self$out[[name]]$timing <- c(self$out[[name]]$timing, ret$theTime)
+                    names(self$out[[name]]$timing)[length(self$out[[name]]$timing)] <- MCMCnamesToUse
+                    alreadyParamNames <- dimnames(self$out[[name]]$summary)[[3]]
+                    newSummary <- ret$summary[self$statNames0,alreadyParamNames]  ## reorder
+                    newSummary3D <- array(as.numeric(newSummary), c(1,dim(newSummary)[1],dim(newSummary)[2]))
+                    dimnames(newSummary3D) <- list(MCMCnamesToUse, self$statNames0, alreadyParamNames)
+                    self$out[[name]]$summary <- abind(self$out[[name]]$summary, newSummary3D, along=1)
+                    newESS <- ret$summary['effectiveSize',]
+                    newESS <- newESS[alreadyParamNames]    ## reorder
+                    oldMCMCnames <- dimnames(self$out[[name]]$ESS)[[1]]
+                    self$out[[name]]$ESS <- rbind(self$out[[name]]$ESS, newESS)
+                    dimnames(self$out[[name]]$ESS)[[1]] <- c(oldMCMCnames, MCMCnamesToUse)
+                    newE <- newESS / ret$theTime   ## newESS already reordered
+                    self$out[[name]]$Efficiency <- rbind(self$out[[name]]$Efficiency, newE)
+                    dimnames(self$out[[name]]$Efficiency)[[1]] <- c(oldMCMCnames, MCMCnamesToUse)
+                } else stop('bad first argument')
+            })
+            totTime <- round(t[3]/60, 0)
+            message('name:  ', name)
+            message('MCMCs: ', MCMCsToPrint)
+            message(totTime, ' minute', if(totTime==1) '' else 's')
+            message('*******************************************************')
+            message('*******************************************************')
+            return(invisible(NULL))
         },
         newListEntry = function(paramNames) {
             l <- list()
--- run.R
+++ run.R
@@ -9,22 +9,15 @@ load('models.RData')
 
 results <- resultsObjectDef(niter=niter)
 
-results$run('dipper', dipper,     MCMCs = c('nimble','nimble_slice','jags','autoBlock'))
-results$run('dipper', dipperDHMM, MCMCs = c('nimble','nimble_slice','autoBlock'), MCMCnames = c('nimbleDHMM','nimble_sliceDHMM','autoBlockDHMM'))
-results$run('dipper', dipperCJS,  MCMCs = c('nimble','nimble_slice'), MCMCnames = c('nimbleCJS','nimble_sliceCJS'))
-results$run('orchid', orchidDHMM, MCMCs = c('nimble','nimble_slice','autoBlock'), MCMCnames = c('nimbleDHMM','nimble_sliceDHMM','autoBlockDHMM'), monitors = c('s','psiV','psiF','psiD'))
-results$run('orchid', orchidJAGSfunction,                             MCMCnames = c('jags'))
-results$run('goose',  gooseDHMM,  MCMCs = c('nimble','nimble_slice','autoBlock'), MCMCnames = c('nimbleDHMM','nimble_sliceDHMM','autoBlockDHMM'), monitors = c('p','phi','psi'))
+results$run('dipper', dipper,     MCMCs = c('nimble','autoBlock','jags'))
+results$run('dipper', dipperCJS,  MCMCs = c('nimble','autoBlock'), MCMCnames = c('nimbleCJS', 'autoBlockCJS'))
+results$run('orchid', orchidDHMM, MCMCs = c('nimble','autoBlock'), MCMCnames = c('nimbleDHMM','autoBlockDHMM'), monitors = c('s','psiV','psiF','psiD'))
+results$run('orchid', orchidJAGSfunction,                          MCMCnames = c('jags'))
+results$run('goose',  gooseDHMM,  MCMCs = c('nimble','autoBlock'), MCMCnames = c('nimbleDHMM','autoBlockDHMM'), monitors = c('p','phi','psi'))
+##out<-results$out; save(out, file = '~/GitHub/userDistMCMC/results.RData')
+results$run('goose',  gooseExpJAGSfunction,                        MCMCnames = c('jags'))
 
-out <- results$out
-save(out, file = '~/GitHub/userDistMCMC/results.RData')
-
-results$run('goose',  gooseExpJAGSfunction,                           MCMCnames = c('jags'))
-
-out <- results$out
-save(out, file = '~/GitHub/userDistMCMC/results.RData')
+out<-results$out; save(out, file = '~/GitHub/userDistMCMC/results.RData')
 
 
-##results$run(dipperSeasonalDHMM, MCMCs = 'nimble')   ## stopped using
-
-
+##results$run('dipper', dipperDHMM, MCMCs = c('nimble','autoBlock'), MCMCnames = c('nimbleDHMM','autoBlockDHMM'))
