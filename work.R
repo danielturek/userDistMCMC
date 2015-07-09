c442c2c04795bb513dcc085889523c794036123f
danielturek <dturek@berkeley.edu>
2015-07-09 10:47:45 -0700

    fast run

--- work.R
+++ work.R
@@ -5,12 +5,7 @@ source('defs.R')
 load('~/GitHub/userDistMCMC/results.RData')
 ##load('~/GitHub/userDistMCMC/resultsNew.RData')
 
-results_check(out)   ### eventually can DELETE this line
 
-results_plot(out)   ### eventually can DELETE this line
-
-
-results_check(results$out)   ### eventually use this line instead
-
-results_plot(results$out)   ### eventually use this line instead
+results_check(results$out)
+results_plot(results$out)
 
