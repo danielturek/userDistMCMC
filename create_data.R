81b17ddc3e6d1af575453f9e94b7ce41d8fd1ee7
danielturek <dturek@berkeley.edu>
2015-09-09 20:34:50 -0700

    initial figure making

--- create_data.R
+++ create_data.R
@@ -89,22 +89,16 @@ dipperCJS <- list(code=code, constants=constants, data=data, inits=inits)
 code <- quote({
     phi ~ dunif(0, 1)
     p ~ dunif(0, 1)
-    log_phi <- log(phi)
-    log_pp <- log(p)
-    log_1minusP <- log(1-p)
-    ##for (i in 1:nind) {
-    ##    y[i, first[i]:k] ~ dCJS(length=k-first[i]+1, last=last[i]-first[i]+1, phi=phi, p=p)
-    ##}
     chi[k] <- 1
     for(j in 1:(k-1))
         chi[k-j] <- (1-phi) + phi * (1-p) * chi[k-j+1]
     for(j in 1:k)
         logChi[j] <- log(chi[j])
     for(i in 1:nind)
-        onesVector[i] ~ dCJS2(length=length[i], nSightingsMinus1=nSightingsMinus1[i], nNonSightings=nNonSightings[i], log_phi=log_phi, log_pp=log_pp, log_1minusP=log_1minusP, logChi=logChi[last[i]])
+        zerosVector[i] ~ dCJS2(length=length[i], nSightingsMinus1=nSightingsMinus1[i], nNonSightings=nNonSightings[i], log_phi=log(phi), log_pp=log(p), log_1minusP=log(1-p), logChi=logChi[last[i]])
 })
 constants  <- list(k=k, nind=nind, length=last-first, nSightingsMinus1=nSightings-1, nNonSightings=last-first-nSightings+1, last=last)
-data       <- list(onesVector=rep(1,nind))
+data       <- list(zerosVector=rep(0,nind))
 inits      <- list(phi=0.6, p=0.9)
 dipperCJS2 <- list(code=code, constants=constants, data=data, inits=inits)
 
@@ -175,16 +169,16 @@ dipperPoissonJAGSfunction <- function(niter = 100000) {
 model {
     phi ~ dunif(0, 1)
     p ~ dunif(0, 1)
-    for(i in 1:nind) {
-        logL[i] <- -1*(last[i]-first[i])*log(phi) - (nSightings[i]-1)*log(p) - (last[i]-first[i]-nSightings[i]+1)*log(1-p) - logChi[last[i]]
-        zerosVector[i] ~ dpois(logL[i])
-    }
     chi[k] <- 1
     for(j in 1:(k-1)) {
         chi[k-j] <- (1-phi) + phi * (1-p) * chi[k-j+1]
     }
     for(j in 1:k) {
         logChi[j] <- log(chi[j])
+    }
+    for(i in 1:nind) {
+        logL[i] <- -1*length[i]*log(phi) - nSightingsMinus1[i]*log(p) - nNonSightings[i]*log(1-p) - logChi[last[i]]
+        zerosVector[i] ~ dpois(logL[i])
     }
 }
 ',fill=TRUE) 
@@ -195,7 +189,8 @@ model {
     nSightings <- apply(y, 1, function(hist) sum(hist, na.rm=TRUE))
     zerosVector <- rep(0,nind)
     if(trunc) { ind <- 1:3;   nind<-length(ind);   first<-first[ind];   last<-last[ind];   y<-y[ind,,drop=FALSE];   nSightings<-nSightings[ind];   zerosVector<-zerosVector[ind] }
-    jags_data <- list(zerosVector=zerosVector, k=k, nind=nind, first=first, last=last, nSightings=nSightings)
+    constants  <- list(, last=last)
+    jags_data <- list(zerosVector=zerosVector, k=k, nind=nind, length=last-first, nSightingsMinus1=nSightings-1, nNonSightings=last-first-nSightings+1, last=last)
     initFunction <- function() list(phi=0.6, p=0.9)
     parameters <- c('p', 'phi')
     library(R2WinBUGS) 
