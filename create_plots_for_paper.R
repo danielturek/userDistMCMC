c26e33f86a2117f4b3f48fdfd66766075a8cd57f
danielturek <danielturek@gmail.com>
2016-05-30 17:20:17 +0900

    updated paper plots for revision

--- create_plots_for_paper.R
+++ create_plots_for_paper.R
@@ -1,5 +1,62 @@
+## NEW FIGRURES for revision of paper for EES journal
+## this will also include scatterplot or boxplot of values
+library(ggplot2)
+library(dplyr)
+load('~/github/userDistMCMC/results.RData')
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+    library(grid)
+    plots <- c(list(...), plotlist)
+    numPlots = length(plots)
+    if (is.null(layout)) layout <- matrix(seq(1, cols * ceiling(numPlots/cols)), ncol = cols, nrow = ceiling(numPlots/cols))
+    if (numPlots==1) { print(plots[[1]])
+    } else { grid.newpage()
+             pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+             for (i in 1:numPlots) {
+                 matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+                 print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row, layout.pos.col = matchidx$col))
+             }
+         }
+}
+df <- results$df
+df$mcmc <- as.character(df$mcmc)   ## make 'mcmc' column as character strings
+df[df$model=='dipper' & df$mcmc=='nimble', ]$mcmc <- 'Latent State'
+df[df$model=='dipper' & df$mcmc=='jags', ]$mcmc <- 'Latent State (JAGS)'
+df[df$model=='dipper' & df$mcmc=='nimbleCJS2', ]$mcmc <- 'Filtering'
+df[df$model=='dipper' & df$mcmc=='jagsPoisson', ]$mcmc <- 'Filtering (JAGS)'
+df[df$model=='orchid' & df$mcmc=='jags', ]$mcmc <- 'Latent State (JAGS)'
+df[df$model=='orchid' & df$mcmc=='nimbleDHMM2', ]$mcmc <- 'Filtering'
+df[df$model=='orchid' & df$mcmc=='autoBlockDHMM2', ]$mcmc <- 'Filtering & Blocking'
+df[df$model=='goose'  & df$mcmc=='jagsExp', ]$mcmc <- 'Latent State (JAGS)'
+df[df$model=='goose'  & df$mcmc=='nimbleDHMM', ]$mcmc <- 'Filtering (RR)'
+df[df$model=='goose'  & df$mcmc=='autoBlockDHMM', ]$mcmc <- 'Filtering & Blocking (RR)'
+dipperMCMCs <- c('Latent State', 'Latent State (JAGS)', 'Filtering', 'Filtering (JAGS)')
+orchidMCMCs <- c('Latent State (JAGS)', 'Filtering', 'Filtering & Blocking')
+gooseMCMCs <- c('Latent State (JAGS)', 'Filtering (RR)', 'Filtering & Blocking (RR)')
+dipperWidth <- 7
+orchidWidth <- 7
+gooseWidth  <- 7
+makePaperPlot2 <- function(model_arg, mcmcs, thiswidth, thisdf) {
+    thisdf <- filter(thisdf, model == model_arg)
+    thisdf$mcmc <- as.factor(thisdf$mcmc)
+    thisdf <- filter(thisdf, mcmc %in% mcmcs)
+    thisdf$mcmc <- factor(as.character(thisdf$mcmc), levels=mcmcs)   ## re-order, according to mcmcs argument
+    p1 <- ggplot(thisdf, aes(mcmc, Efficiency, fill=mcmc), xlab='') + stat_summary(fun.y='min', geom='bar') + ggtitle('Minimum') + ylab('Sampling efficiency (ESPS)') + theme(legend.position='none', axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x = element_blank())
+    p2 <- ggplot(thisdf, aes(mcmc, Efficiency, fill=mcmc), xlab='') + stat_summary(fun.y='mean', geom='bar') + ggtitle('Mean') + ylab('Sampling efficiency (ESPS)') + theme(legend.position='none', axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x = element_blank())
+    p3 <- ggplot(thisdf, aes(mcmc, Efficiency, colour=mcmc), xlab='') + geom_point(size=1) + ggtitle('Values') + ylab('Sampling efficiency (ESPS)') + theme(legend.position='none', axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x = element_blank())
+    p4 <- ggplot(thisdf, aes(mcmc, Efficiency, fill=mcmc)) + geom_bar(stat='identity') + theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x = element_blank(), axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank(), legend.position = 'left') + labs(fill='MCMC Algorithm     ')
+    dev.new(width=thiswidth, height=3)
+    multiplot(p1, p2, p3, p4, cols=4)
+    localFile <- paste0('~/github/userDistMCMC/plot_', model_arg, '.pdf')
+    paperFile <- paste0('~/github/nimble/nimblePapers/CR-MCMC/EES_revision/plot_', model_arg, '.pdf')
+    dev.copy2pdf(file=localFile)
+    system(paste0('cp ', localFile, ' ', paperFile))
+}
+makePaperPlot2('dipper', dipperMCMCs, dipperWidth, df)
+makePaperPlot2('orchid', orchidMCMCs, orchidWidth, df)
+makePaperPlot2('goose',  gooseMCMCs,  gooseWidth,  df)
 
 
+## original paper plots for first submissions to Biometrics, EES, etc.
 library(ggplot2)
 library(dplyr)
 load('~/github/userDistMCMC/results.RData')
